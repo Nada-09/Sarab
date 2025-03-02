e75e01eb0421743d202a4a491e95366fc52b3794
Nada-09 <nadaalmajidi4@gmail.com>
2025-03-02 23:14:20 +0300

    Initial Commit

---
+++ SarabTests/SarabTests.swift
@@ -0,0 +1,17 @@
+//
+//  SarabTests.swift
+//  SarabTests
+//
+//  Created by Nada Abdullah on 02/09/1446 AH.
+//
+
+import Testing
+@testable import Sarab
+
+struct SarabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
